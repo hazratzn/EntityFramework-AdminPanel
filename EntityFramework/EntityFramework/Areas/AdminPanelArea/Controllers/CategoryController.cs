ead0ffcfaed9af45f642775d36a281f35b17e319
hazratzn <hazratzn@code.edu.az>
2022-03-15 12:44:07 -0700

    AdminPanel

---
+++ EntityFramework/EntityFramework/Areas/AdminPanelArea/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using EntityFramework.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Areas.AdminPanelArea.Controllers
+{
+    [Area("AdminPanelArea")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var category = await _context.Categories.ToListAsync();
+            return View(category);
+        }
+        public async Task<IActionResult> Detail(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(m => m.Id == id);
+            return Json(new
+            {
+                categoryName = category.Name,
+                action = "Detail",
+                Id = id
+            });
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            return Json(new
+            {
+                action = "Edit",
+                Id = id
+            });
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Json(new
+            {
+                action = "Delete",
+                Id = id
+            });
+        }
+    }
+}
---
+++ EntityFramework/EntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using EntityFramework.Data;
+using EntityFramework.Models;
+using EntityFramework.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+            SliderDetail details = await _context.SliderDetails.FirstOrDefaultAsync();
+            List<Category> categories = await _context.Categories.ToListAsync();
+            List<Product> products = await _context.Products
+                .Include(m => m.Category)
+                .Include(m => m.Images)
+                .OrderByDescending(m => m.Id)
+                .Skip(1)
+                .Take(8)
+                .ToListAsync();
+            About abouts = await _context.Abouts
+                .Include(i=>i.Advantages)
+                .FirstOrDefaultAsync();
+            ExpertSection expertSection = await _context.ExpertSections.FirstOrDefaultAsync();
+            List<Expert> experts = await _context.Experts
+                .Include(i => i.Image)
+                .ToListAsync();
+            List<Expert> expertsThoughts = await _context.Experts
+                .Include(i => i.Image)
+                .OrderBy(m=>m.Id)
+                .Take(2)
+                .OrderByDescending(m=>m.Id)
+                .ToListAsync();
+            List<Blog> blogs = await _context.Blogs.ToListAsync();
+            List<Instagram> instagrams = await _context.Instagrams.ToListAsync();
+
+
+
+            HomeVM homeVM = new HomeVM
+            {
+                Sliders = sliders,
+                Detail = details,
+                Categories = categories,
+                Products = products,
+                Abouts = abouts,
+                Experts = experts,
+                ExpertsThoughts =expertsThoughts,
+                ExpertSection = expertSection,
+                Blogs = blogs,
+                Instagrams = instagrams
+            };
+            return View(homeVM);
+        }
+    }
+}
